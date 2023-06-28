c6601b05a2cfadb6209fbb7074d62e5cc01da9de
m0ima-t3rris <m0ima.t3rris@gmail.com>
2023-06-27 17:17:02 -0700

    po

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,20 +1,20 @@
 #include "main.h"
 /**
- * print_rev - print a string in reverse fillowed by new line.
+ * print_rev - prints a string, in reverse, followed by a new line.
  * @s: string
  * return: 0
  */
 void print_rev(char *s)
 {
 int lol = 0;
-int t;
+int o;
 while (*s != '\0')
 {
 lol++;
 s++;
 }
 s--;
-for (r = lol; r > 0; r--)
+for (o = lol; o > 0; o--)
 {
 _putchar(*s);
 s--;

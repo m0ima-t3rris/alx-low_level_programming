56a83550b5d8827f31866979e54de677d0aab066
m0ima-t3rris <m0ima.t3rris@gmail.com>
2023-07-04 19:01:28 -0700

    out

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+ * main - entry point, 
+ * print alphabet in small cases.
+ * return: 0 sucess.
+ */
+int main(void)
+{
+int tk;
+for (tk = 'a' ; tk <= 'z' ; tk++)
+{
+putchar(tk);
+}
+putchar('\n');
+return (0);
+}

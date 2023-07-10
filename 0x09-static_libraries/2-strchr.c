dbeff303e5509fdf8c1d4b5e6b734b898686e274
m0ima-t3rris <m0ima.t3rris@gmail.com>
2023-07-09 19:25:23 -0700

    lol

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -1,17 +1,1 @@
-#include "main.h"
-/**
- * _strchr - Entry point, locate a charector in string.
- * @s: input.
- * @c: input.
- * Return: Always 0 (Success)
- */
-char *_strchr(char *s, char c)
-{
-int r = 0;
-for (; s[r] >= '\0'; i++)
-{
-if (s[r] == c)
-return (&s[r]);
-}
-return (0);
-}
+
